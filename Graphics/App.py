4fa0d88f3f43f75fb1faadfcd36956a7f25721ad
Marco <m.herrera0799@gmail.com>
2020-10-21 00:28:12 -0600

    Fix concurrency bug

--- Graphics/App.py
+++ Graphics/App.py
@@ -21,12 +21,19 @@ class App(QWidget):
         self.step_button = QPushButton("Step")
         self.exec_button = QPushButton("Execute")
         self.pause_button = QPushButton("Pause")
+
+        self.lock1_button = QPushButton("lock")
+        self.lock2_button = QPushButton("lock")
+        self.lock3_button = QPushButton("lock")
+        self.lock4_button = QPushButton("lock")
+
         self.processors = []
         self.caches = []
         self.instructions = [None, None, None, None]
         self.next_instructions = [None, None, None, None]
         self.memory = None
         self.threads = []
+        self.locks = [False, False, False, False]
         self.init_ui()
 
     def init_ui(self):
@@ -51,10 +58,10 @@ class App(QWidget):
         processor_view_label = QLabel("Processor View")
         processor_view_label.setStyleSheet("font-size:15pt; font-weight:600; color:#000000;")
         self.layout.addWidget(processor_view_label, 0, 1, 1, 4, Qt.AlignCenter)
-        self.layout.addWidget(QLabel("Processor: 0"), 2, 1, 1, 2, Qt.AlignCenter)
-        self.layout.addWidget(QLabel("Processor: 1"), 2, 3, 1, 2, Qt.AlignCenter)
-        self.layout.addWidget(QLabel("Processor: 2"), 6, 1, 1, 2, Qt.AlignCenter)
-        self.layout.addWidget(QLabel("Processor: 3"), 6, 3, 1, 2, Qt.AlignCenter)
+        self.layout.addWidget(QLabel("Processor: 0"), 2, 1, 1, 1, Qt.AlignCenter)
+        self.layout.addWidget(QLabel("Processor: 1"), 2, 3, 1, 1, Qt.AlignCenter)
+        self.layout.addWidget(QLabel("Processor: 2"), 6, 1, 1, 1, Qt.AlignCenter)
+        self.layout.addWidget(QLabel("Processor: 3"), 6, 3, 1, 1, Qt.AlignCenter)
 
         self.layout.addWidget(self.cache_views[0], 5, 1, 1, 2, Qt.AlignCenter)
         self.layout.addWidget(self.cache_views[1], 5, 3, 1, 2, Qt.AlignCenter)
@@ -73,8 +80,31 @@ class App(QWidget):
         self.pause_button.clicked.connect(lambda: self.pause_execution())
         self.pause_button.setMinimumWidth(175)
 
+        self.layout.addWidget(self.lock1_button, 2, 2, alignment=Qt.AlignCenter)
+        self.lock1_button.clicked.connect(lambda: self.lock(1))
+        self.layout.addWidget(self.lock2_button, 2, 4, alignment=Qt.AlignCenter)
+        self.lock2_button.clicked.connect(lambda: self.lock(2))
+        self.layout.addWidget(self.lock3_button, 6, 2, alignment=Qt.AlignCenter)
+        self.lock3_button.clicked.connect(lambda: self.lock(3))
+        self.layout.addWidget(self.lock4_button, 6, 4, alignment=Qt.AlignCenter)
+        self.lock4_button.clicked.connect(lambda: self.lock(4))
+
         self.setLayout(self.layout)
         self.show()
+
+    def lock(self, index):
+        self.locks[index - 1] = not self.locks[index - 1]
+        color = "light gray"
+        if self.locks[index - 1]:
+            color = "red"
+        if index == 1:
+            self.lock1_button.setStyleSheet("background-color:" + color)
+        elif index == 2:
+            self.lock2_button.setStyleSheet("background-color:" + color)
+        elif index == 3:
+            self.lock3_button.setStyleSheet("background-color:" + color)
+        elif index == 4:
+            self.lock4_button.setStyleSheet("background-color:" + color)
 
     def thread_execute(self):
         # self.execute()
@@ -101,7 +131,7 @@ class App(QWidget):
         i = 0
         self.memory.tick()
         for processor in self.processors:
-            if processor.busy or len(processor.instructions) == 0:
+            if processor.busy or len(processor.instructions) == 0 or self.locks[i]:
                 i += 1
                 continue
             if self.instructions[i].text() == "":
@@ -178,6 +208,7 @@ class App(QWidget):
             data = QTableWidgetItem('0x' + '{:04x}'.format(memory.content[i]).upper())
             data.setTextAlignment(Qt.AlignCenter)
             self.memory_view.setItem(i, 1, data)
+        self.memory_view.update()
 
     def init_cache_view(self, cache_blocks=4):
         cache_view = QTableWidget()
--- Model/Memory.py
+++ Model/Memory.py
@@ -1,4 +1,5 @@
 import time
+from threading import RLock
 
 
 class Memory:
@@ -10,6 +11,7 @@ class Memory:
         self.latency = latency
         self.ticks = 0
         self.content = [0] * num_blocks
+        self.lock = RLock()
 
     def __str__(self):
         return str(self.content)
@@ -18,22 +20,23 @@ class Memory:
         self.ticks += 1
 
     def write(self, address, data):
-        print("Memory Written")
-        self.ticks = 0
-        if address >= self.num_blocks:
-            print("Write error: Address out of range")
-        while self.ticks < self.latency:
-            time.sleep(0.1)
-            continue
-        self.ticks = 0
-        print(address)
-        self.content[address] = data
+        with self.lock:
+            self.ticks = 0
+            if address >= self.num_blocks:
+                print("Write error: Address out of range")
+            while self.ticks < self.latency:
+                time.sleep(0.1)
+                continue
+            self.ticks = 0
+            self.content[address] = data
+
 
     def read(self, address):
-        self.ticks = 0
-        if address >= self.num_blocks:
-            print("Read error: Address out of range")
-        while self.ticks < self.latency:
-            continue
-        self.ticks = 0
-        return self.content[address]
+        with self.lock:
+            self.ticks = 0
+            if address >= self.num_blocks:
+                print("Read error: Address out of range")
+            while self.ticks < self.latency:
+                continue
+            self.ticks = 0
+            return self.content[address]
